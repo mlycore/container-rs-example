8cff9b92d0a54eb0a0591b12ddb16768fb4112ab
mlycore <maxwell92@126.com>
2022-05-05 20:26:49 +0800

    feat(main): introducing tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use command::command::new_command;
+use tracing::Level;
+
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+    .with_max_level(Level::INFO)
+    .init();
+    new_command(); 
 }
