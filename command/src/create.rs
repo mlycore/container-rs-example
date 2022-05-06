afefe3caf03725f5889ee12fd1ca80c637335949
mlycore <maxwell92@126.com>
2022-05-06 22:57:29 +0800

    wip: add clone for container create

--- command/src/create.rs
+++ command/src/create.rs
@@ -6,6 +6,11 @@ use std::io::Read;
 use spec::spec::Spec;
 // use serde::{Deserialize, Deserialize};
 
+use nix::{
+    unistd::Pid,
+    sched::{CloneFlags, clone},
+};
+
 pub struct CreateCommand {
     pub id: String,
     pub oci_config: String,
@@ -30,6 +35,39 @@ impl CreateCommand {
         file.read_to_string(&mut spec_json).unwrap();
         let spec: Spec = serde_json::from_str(&spec_json).unwrap();
         info!("spec_json: {:?}", spec);    
+
+        let namespaces: Vec<Namespace> = match &spec.linux {
+            Some(linux) => linux.namespaces.clone().unwrap_or(Vec::new()),
+            None => Vec::new(),
+        }
+
+        //TODO: consider use expect or unwrap_err
+        let pid = fork().unwrap();
     }
 }
 
+pub fn fork(namespaces: &Vec<Namespace>) -> Result<Pid> {
+    const STACK_SIZE: usize = 1024 * 1024 * 4;
+
+    let spec_namespaces = namespaces.into_iter().map(|ns| to_flags(ns)).reduce(|a, b| a | b);
+    let clone_flags = match spec_namespaces {
+        Some(flags) => flags,
+        None => CloneFlags::empty(),
+    };
+    //FIXME; add func for exec
+    let process = clone(Box::new(), stack, clone_flags, None);
+    process 
+}
+
+fn to_flags(namespace: &Namespace) -> CloneFlags {
+    match namespace.namespace.as_str() {
+        "pid" => CloneFlags::CLONE_NEWPID,
+        "network" => CloneFlags::CLONE_NEWNET,
+        "mount" => CloneFlags::CLONE_NEWNS,
+        "ipc" => CloneFlags::CLONE::NEWIPC,
+        "uts" => CloneFlags::CLONE_NEWUTS,
+        "user" => CloneFlags::CLONE_NEWUSER,
+        "cgroup" => CloneFlags::CLONE_NEWCGROUP,
+        _ => panic!("unknown namespace {}", namespace.namespace),
+    }
+}
